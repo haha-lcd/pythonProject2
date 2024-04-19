9cdf554815fa35823cc93341984e4d3653f3f697
haha-lcd <wangyiliuchengdong@163.com>
2024-04-19 15:36:02 +0800

    test other files

--- tests/pytest_test.py
+++ tests/pytest_test.py
@@ -1,5 +1,5 @@
 import pytest
-import haha.haha as ha
+# import haha.haha as ha
 
 import haha.haha
 
@@ -8,7 +8,7 @@ def test_01():
     print("this is {} testcase".format(__name__))
 
 def test_hahaha():
-    assert ha.haha_add(1,2) ==3
+    assert haha_add(1,2) ==3
 
 
 if __name__=='__main__':
